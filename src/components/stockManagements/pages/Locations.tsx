318298aefe6982da0ac2b9b959dd93dc8ac35031
Fella-Dahmani <f.dahmani.dj@outlook.fr>
2024-08-04 14:47:32 -0400

    updated some Navbar css

--- src/components/stockManagements/pages/Locations.tsx
+++ src/components/stockManagements/pages/Locations.tsx
@@ -43,6 +43,89 @@ const Locations = () => {
                     </tr>
                 </tbody>
             </table>
+
+            
+            <div className="subTitleContainerlocation">
+                <h2 className="subtitle">Par categorie</h2>
+                <input type="text" placeholder="Recherche" />
+                <button>Appliquer les filtres</button>
+            </div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Code</th>
+                        <th>Description</th>
+                        <th>Categorie</th>
+                        <th>Quantite</th>
+                        <th>Fournisseur</th>
+                        <th>Prix</th>
+                        <th>Seuil Critique</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>5</td>
+                        <td>6</td>
+                        <td>7</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>5</td>
+                        <td>6</td>
+                        <td>7</td>
+                    </tr>
+                </tbody>
+            </table>
+
+
+            
+            <div className="subTitleContainerlocation">
+                <h2 className="subtitle">Par emplacement</h2>
+                <input type="text" placeholder="Recherche" />
+                <button>Appliquer les filtres</button>
+            </div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Code</th>
+                        <th>Description</th>
+                        <th>Categorie</th>
+                        <th>Quantite</th>
+                        <th>Fournisseur</th>
+                        <th>Prix</th>
+                        <th>Seuil Critique</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>5</td>
+                        <td>6</td>
+                        <td>7</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>5</td>
+                        <td>6</td>
+                        <td>7</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 };
