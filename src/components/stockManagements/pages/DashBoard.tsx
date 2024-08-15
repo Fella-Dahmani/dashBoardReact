9c87e9441889c1f92093c0e1a20e5c42ab253931
ChaosCrider <ChaosCrider@hotmail.com>
2024-08-15 12:06:41 -0400

    Added basic cards to the dashboard component

--- src/components/stockManagements/pages/DashBoard.tsx
+++ src/components/stockManagements/pages/DashBoard.tsx
@@ -1,17 +1,76 @@
 import React, { useState } from "react";
 
+
+
+
+
 const [activeForm, setActiveForm] = useState <
-    "ventes" | "achats" | null >(null);
+    "ventes" | "achats" | "ruptures" | "surstocks" | null 
+>(null);
 
-const ventes = () => {
 
-}
+
+const handleCardClick = (
+    formType: "ventes" | "achats" | "ruptures" | "surstocks"
+) => {
+    setActiveForm(formType);
+};
+
+
+
+
+
 
 const DashBoard = () => {
     return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
+        <>
+            <div>
+                <h1>Dashboard</h1>
+            </div>
+            
+            <div className="CardConatiner">
+                <div
+                    className={`card ${activeForm === "ventes" ? "clicked" : ""}`}
+                    onClick={() => handleCardClick("ventes")}
+                >
+                    <h3>Les ventes</h3>
+                    <div className="ventesStats"></div>
+                </div>
+            </div>
+
+            <div className="CardConatiner">
+                <div
+                    className={`card ${activeForm === "achats" ? "clicked" : ""}`}
+                    onClick={() => handleCardClick("achats")}
+                >
+                    <h3>Les ventes</h3>
+                    <div className="achatsStats"></div>
+                </div>
+            </div>
+
+            <div className="CardConatiner">
+                <div
+                    className={`card ${activeForm === "ruptures" ? "clicked" : ""}`}
+                    onClick={() => handleCardClick("ruptures")}
+                >
+                    <h3>Les ventes</h3>
+                    <div className="vrupturesStats"></div>
+                </div>
+            </div>
+
+            <div className="CardConatiner">
+                <div
+                    className={`card ${activeForm === "surstocks" ? "clicked" : ""}`}
+                    onClick={() => handleCardClick("surstocks")}
+                >
+                    <h3>Les ventes</h3>
+                    <div className="surstocksStats"></div>
+                </div>
+            </div>
+
+
+        </>
+
     );
 };
 
