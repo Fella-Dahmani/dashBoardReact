9f4af211e4fb0a6fff2ccdf7583b24ca13db2221
ChaosCrider <ChaosCrider@hotmail.com>
2024-08-19 07:29:41 -0400

    Update DashBoard.tsx
    undo merge made by accident

--- src/components/stockManagements/pages/DashBoard.tsx
+++ src/components/stockManagements/pages/DashBoard.tsx
@@ -1,13 +1,27 @@
+<<<<<<< HEAD
+=======
+import React, { useState } from "react";
 
+const [activeForm, setActiveForm] = useState <
+    "ventes" | "achats" | null >(null);
+
+const ventes = () => {
+>>>>>>> parent of 9c87e94 (Added basic cards to the dashboard component)
+
+}
 
 const DashBoard = () => {
     return (
         <div>
+<<<<<<< HEAD
             <h1 className="title">Dashboard</h1><div className="subTitleContainer">
                
             </div>
             
 
+=======
+            <h1>Dashboard</h1>
+>>>>>>> parent of 9c87e94 (Added basic cards to the dashboard component)
         </div>
     );
 };
