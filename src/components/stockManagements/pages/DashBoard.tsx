fb6cfc5de74ac16af4822e7bd38aa04fb6aebe10
ChaosCrider <ChaosCrider@hotmail.com>
2024-08-19 07:29:03 -0400

    Revert "Merge branch 'Dashboard' into main"
    This reverts commit 41213da61d7c55e2b333699ad802ca2496b1113d, reversing
    changes made to 79fbff6faccce03762e7e9fd0613a51d0e7e77dd.

--- src/components/stockManagements/pages/DashBoard.tsx
+++ src/components/stockManagements/pages/DashBoard.tsx
@@ -1,75 +1,14 @@
-import React, { useState } from "react";
-
-
-
-
-
-const [activeForm, setActiveForm] = useState <
-    "ventes" | "achats" | "ruptures" | "surstocks" | null 
->(null);
-
-
-
-const handleCardClick = (
-    formType: "ventes" | "achats" | "ruptures" | "surstocks"
-) => {
-    setActiveForm(formType);
-};
-
-
-
-
 
 
 const DashBoard = () => {
     return (
-        <>
-            <div>
-                <h1>Dashboard</h1>
+        <div>
+            <h1 className="title">Dashboard</h1><div className="subTitleContainer">
+               
             </div>
             
-            <div className="CardConatiner">
-                <div
-                    className={`card ${activeForm === "ventes" ? "clicked" : ""}`}
-                    onClick={() => handleCardClick("ventes")}
-                >
-                    <h3>Les ventes</h3>
-                    <div className="ventesStats"></div>
-                </div>
-            </div>
 
-            <div className="CardConatiner">
-                <div
-                    className={`card ${activeForm === "achats" ? "clicked" : ""}`}
-                    onClick={() => handleCardClick("achats")}
-                >
-                    <h3>Les ventes</h3>
-                    <div className="achatsStats"></div>
-                </div>
-            </div>
-
-            <div className="CardConatiner">
-                <div
-                    className={`card ${activeForm === "ruptures" ? "clicked" : ""}`}
-                    onClick={() => handleCardClick("ruptures")}
-                >
-                    <h3>Les ventes</h3>
-                    <div className="vrupturesStats"></div>
-                </div>
-            </div>
-
-            <div className="CardConatiner">
-                <div
-                    className={`card ${activeForm === "surstocks" ? "clicked" : ""}`}
-                    onClick={() => handleCardClick("surstocks")}
-                >
-                    <h3>Les ventes</h3>
-                    <div className="surstocksStats"></div>
-                </div>
-            </div>
-
-
-        </>
+        </div>
     );
 };
 
