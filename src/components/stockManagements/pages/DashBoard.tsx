197adb26d5d8b1c11f29a3bdd39c521f3a62ae9a
ChaosCrider <ChaosCrider@hotmail.com>
2024-09-03 16:24:57 -0400

    adjusted host url

--- src/components/stockManagements/pages/DashBoard.tsx
+++ src/components/stockManagements/pages/DashBoard.tsx
@@ -232,7 +232,10 @@ const DashBoard = () => {
     // call to the BDD
     const syncData = () => {
 
-        const host = "http://localhost:8080/"
+        const host = "https://stockvisiobackend.onrender.com/"
+/*         const host = "http://localhost:8080/" */
+
+
         const rootApi = "api/"
         const baseURL = host + rootApi
         var targetUrl = ""
